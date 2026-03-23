ca1b19dbd758fd4641d55b527395ebea304be0a4
cse-aditya-maurya <cse.aditya.maurya@gmail.com>
2026-03-23 09:39:57 +0530

    Auth service

---
+++ finflow-auth-service/src/main/java/com/finflow/auth/FinflowAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finflow.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinflowAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinflowAuthServiceApplication.class, args);
+	}
+
+}
